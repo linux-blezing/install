cdfbbe040760853522a2ebbfd97fa8980fd71f6d
Al Muhdil Karim <almuhdilkarim2@gmail.com>
2025-11-25 18:48:06 +0700

    update

--- prep.sh
+++ prep.sh
@@ -13,19 +13,24 @@ if [[ ! -z $(findmnt --mountpoint /mnt) ]]; then
  	umount -R /mnt
 fi
 
+if [[ ! -e /dev/mapper/proc ]]; then 
+	cryptsetup luksOpen $DISKPROC proc
+fi
 
-mkfs.ext4 -F -q -b 4096 $DISKPROC &&
-mkfs.ext4 -F -q -b 4096 $DISKDATA &&
+if [[ ! -e /dev/mapper/data  ]]; then 
+	cryptsetup luksOpen $DISKDATA data
+fi
+
+
 mkfs.vfat -F32 -S 4096 -n BOOT $DISKBOOT &&
+mkfs.ext4 -F -b 4096 /dev/mapper/proc &&
+mkfs.ext4 -F -b 4096 /dev/mapper/data &&
 
-mount $DISKPROC /mnt &&
-
-mkdir -p /mnt/boot && 
+mount /dev/mapper/proc /mnt &&
+mkdir -p /mnt/boot &&
 mount -o uid=0,gid=0,dmask=007,fmask=007 $DISKBOOT /mnt/boot/ &&
-
 mkdir -p /mnt/home &&
-mount $DISKDATA /mnt/home &&
-
+mount /dev/mapper/data /mnt/home &&
 
 
 # PROCESSOR
